e62279ba889f01ae32b2dc9fd3b259ad328b16c8
test test <jo@jo>
2018-07-10 16:08:47 +0200

    git router

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,5 @@
 <template>
 <div id="app">
-  <!-- <h1></h1>
-
-
-   -->
-  <v-app>
-    <v-content>
-      <v-btn color="success">{{message}}</v-btn>
-    </v-content>
-  </v-app>
-
-  <v-slider></v-slider>
-
-
+  <router-view></router-view>
 </div>
 </template>
-
-<script>
-export default {
-  name: 'app',
-  data() {
-    return {
-      message: 'Welcome this is Vuetify  Parcel'
-    }
-  }
-}
-</script>
-
-<style lang="scss">
-
-</style>
--- src/main.js
+++ src/main.js
@@ -1,29 +1,23 @@
 import Vue from 'vue';
+import routes from './routes'
+import VueRouter from 'vue-router'
 import Vuetify from 'vuetify'
 import App from './App.vue'
+import Test from './components/Test.vue';
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [{
+    path: '/route-one',
+    component: Test
+  }]
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
-});
-
-
-
-
-
-
-
-// import Vue from 'vue'
-// import App from './App'
-// import Vuetify from 'vuetify'
-//
-//
-// Vue.use(Vuetify)
-//
-// new Vue({
-//   el: '#app',
-//   render: h => h(App)
-// })
+})
